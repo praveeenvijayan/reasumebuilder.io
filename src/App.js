f3811d545194736cf2435fbbdd22973d10e933cd
praveeenvijayan <textpraveennair@gmail.com>
2022-07-02 16:35:19 +0530

    docker testing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         {`Hellooooo there ${process.env.REACT_APP_NAME}....`}
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
